20da99d425ed18d7ddbb2ca4e9dea00f1b704f25
David E. Barrera <dbarrerap@live.com>
2022-07-15 17:19:17 -0500

    Agregar estilo

--- src/app/components/example-table/example-table.component.ts
+++ src/app/components/example-table/example-table.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ExampleTableService } from './example-table.service';
-import { Workbook } from 'exceljs';
+import { Fill, Workbook } from 'exceljs';
 import * as fs from 'file-saver'
 
 @Component({
@@ -28,10 +28,54 @@ export class ExampleTableComponent implements OnInit {
   }
 
   dlExcel() {
+    // Generar el Excel
     let wb = new Workbook()
     let ws = wb.addWorksheet('User Data')
     let header = Object.keys(this.data[0])
     ws.addRow(header)
+
+    ws.eachRow((r, rN) => {
+      r.eachCell((c, cN) => {
+        c.font = {
+          bold: true
+        }
+        c.alignment = {
+          horizontal: 'center',
+          vertical: 'middle'
+        }
+      })
+    })
+
+    let azul : Fill = {
+      type: 'pattern',
+      pattern: 'solid',
+      fgColor: { argb:'b8daff' }
+    }
+
+    let verde : Fill = {
+      type: 'pattern',
+      pattern: 'solid',
+      fgColor: { argb:'c3e6cb' }
+    }
+
+    let amarillo : Fill = {
+      type: 'pattern',
+      pattern: 'solid',
+      fgColor: { argb:'ffeeba' }
+    }
+
+    // Object.assign(ws.getCell('B1').fill, azul)
+    // Object.assign(ws.getCell('C1').fill, verde)
+    // Object.assign(ws.getCell('D1').fill, amarillo)
+    // Object.assign(ws.getCell('E1').fill, verde)
+    // Object.assign(ws.getCell('F1').fill, azul)
+
+    ws.getCell('B1').fill = azul
+    ws.getCell('C1').fill = verde
+    ws.getCell('D1').fill = amarillo
+    ws.getCell('E1').fill = verde
+    ws.getCell('F1').fill = azul
+    
 
     this.data.forEach((elem: { [x: string]: any; }) => {
       let keys = Object.keys(elem)
@@ -42,6 +86,7 @@ export class ExampleTableComponent implements OnInit {
       ws.addRow(row)
     });
 
+    // Guardar el archivo
     let filename = 'Users'
     wb.xlsx.writeBuffer().then(
       data => {
